8f9b2705211d594cf335b195fb2ca2d3a9fc3aad
MostafaZayed <mostafa.zayed@hypercell.net>
2024-10-02 13:38:18 +0300

    add footer and customize navbar

--- apps/eccomerce-18/src/app/app.component.ts
+++ apps/eccomerce-18/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FaConfig, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   standalone: true,
-  imports: [RouterModule, FontAwesomeModule],
-  selector: 'app-root',
+  imports: [RouterModule, FontAwesomeModule, NavbarComponent, FooterComponent],
+  selector: 'ecom-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- apps/eccomerce-18/src/app/shared/font-awesome-icons.ts
+++ apps/eccomerce-18/src/app/shared/font-awesome-icons.ts
@@ -1,7 +1,14 @@
-import { IconDefinition } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faInstagram, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import {  faShoppingCart, faTruckFast, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-export const fontAwesomeIcons:IconDefinition[] = [
-    faUser
-
+export const fontAwesomeIcons: IconDefinition[] = [
+    faUser,
+    faShoppingCart,
+    faUser,
+    faTwitter,
+    faFacebook,
+    faYoutube,
+    faInstagram,
+    faTruckFast
 ]
